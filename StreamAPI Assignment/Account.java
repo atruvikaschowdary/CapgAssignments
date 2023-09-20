34ec9453cf4963228b4f034c44a2412469ce8310
atruvikaschowdary <145389543+atruvikaschowdary@users.noreply.github.com>
2023-09-20 23:25:05 +0530

    Add files via upload

---
+++ StreamAPI Assignment/Account.java
@@ -0,0 +1,194 @@
+package strpac2;
+
+import java.util.Objects;
+
+
+
+
+public class Account {
+	
+	private int accountNumber;
+
+	private String accountHolderName;
+
+	private int balance;
+
+	private String location;
+
+ 
+
+ 
+
+	public Account(int accountNumber, String accountHolderName, int balance, String location) {
+
+		super();
+
+		this.accountNumber = accountNumber;
+
+		this.accountHolderName = accountHolderName;
+
+		this.balance = balance;
+
+		this.location = location;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public int getAccountNumber() {
+
+		return accountNumber;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public void setAccountNumber(int accountNumber) {
+
+		this.accountNumber = accountNumber;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public String getAccountHolderName() {
+
+		return accountHolderName;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public void setAccountHolderName(String accountHolderName) {
+
+		this.accountHolderName = accountHolderName;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public int getBalance() {
+
+		return balance;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public void setBalance(int balance) {
+
+		this.balance = balance;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public String getLocation() {
+
+		return location;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	public void setLocation(String location) {
+
+		this.location = location;
+
+	}
+
+ 
+
+ 
+
+ 
+
+	@Override
+
+	public int hashCode() {
+
+		return Objects.hash(accountHolderName, accountNumber, balance, location);
+
+	}
+
+ 
+
+ 
+
+ 
+
+	@Override
+
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+
+			return true;
+
+		if (obj == null)
+
+			return false;
+
+		if (getClass() != obj.getClass())
+
+			return false;
+
+		Account other = (Account) obj;
+
+		return Objects.equals(accountHolderName, other.accountHolderName) && accountNumber == other.accountNumber
+
+&& balance == other.balance && Objects.equals(location, other.location);
+
+	}
+
+ 
+
+ 
+
+ 
+
+	@Override
+
+	public String toString() {
+
+		return "Account [accountNumber=" + accountNumber + ", accountHolderName=" + accountHolderName + ", balance="
+
+				+ balance + ", location=" + location + "]";
+
+	}
+
+ 
+
+}
---
+++ StreamAPI Assignment/StreamDemo2.java
@@ -0,0 +1,86 @@
+package strpac2;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+ 
+
+public class StreamDemo2 {
+
+	static List<Account> allAccounts;
+	static Collection<Account> list;
+
+ 
+
+	public static void main(String[] args) {
+
+		  Account a1 = new Account(1,"vikas", 20000, "Pune");
+
+ 
+
+		  Account a2 = new Account(2,"Kumar", 10500, "Hyderabad");
+
+ 
+
+		  Account a3 = new Account(3,"Pavan", 2000, "Mumbai");
+
+ 
+
+		  Account a4 = new Account(4,"Manoj", 6000, "Mumbai");
+
+ 
+
+		  Account a5 = new Account(5,"Kishore", 3500, "Bangalore");
+
+		  allAccounts = Arrays.asList(a1,a2,a3,a4,a5);
+
+//		  list.stream().forEach(e->System.out.println(e));
+
+		  System.out.println(getAccountOfMinBalance());
+		  System.out.println(getAccountOfMaxBalance());
+
+
+ 
+
+	}//end main
+
+	public static Account getAccountOfMaxBalance() {
+		Comparator<Account> com1 = (a1,a2)->{
+			return (a1.getBalance() + a2.getBalance());
+		};
+
+		Account a = 
+				allAccounts.stream().max(com1).get();
+		return a;
+	}
+
+ 
+
+	public static Account getAccountOfMinBalance()
+	{
+
+		Comparator<Account> com = (a1,a2)->{
+			return (a1.getBalance() - a2.getBalance());
+		};
+
+		Account a = 
+				allAccounts.stream().min(com).get();
+		return a;
+
+	}
+}
+
+
+	
+	
+
+		
+
+		
+
+		
+
+		
