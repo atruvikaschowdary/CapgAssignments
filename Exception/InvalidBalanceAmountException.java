2a0d766c5c6dd205c166b93923bcc7f7b38e61be
atruvikaschowdary <145389543+atruvikaschowdary@users.noreply.github.com>
2023-09-24 15:20:46 +0530

    Add files via upload

---
+++ Exception/InvalidBalanceAmountException.java
@@ -0,0 +1,11 @@
+package p1;
+
+
+	public class InvalidBalanceAmountException extends Exception {
+		public InvalidBalanceAmountException(String s) {
+			super(s);
+		}
+
+	}
+
+
---
+++ Exception/InvalidValidUserNameException.java
@@ -0,0 +1,12 @@
+package p1;
+
+
+	
+	public class InvalidValidUserNameException extends Exception {
+		public InvalidValidUserNameException(String msg) {
+			super(msg);
+		}
+
+	}
+
+
